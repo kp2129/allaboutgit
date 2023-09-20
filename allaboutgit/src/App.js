80c38c0dfc7c0521569f9b885d4561665fe4c092
kp2129 <kristapspolis0@gmail.com>
2023-09-20 09:27:28 +0300

    Added text in app.js

--- allaboutgit/src/App.js
+++ allaboutgit/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+    <h1>All about github</h1>
+    <h2>This website will remind you all about github commands</h2>
+    
+    </>
   );
 }
 
